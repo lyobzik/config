14d0ef152772db4258f26d0811afaf92f8c3101a
lyobzik <lyobzik@gmail.com>
2016-01-03 17:04:06 +0600

    Add config creator.

--- config.go
+++ config.go
@@ -3,13 +3,18 @@ import (
 	"path"
 	"strings"
 	"errors"
+	"io/ioutil"
+	"io"
+	"os"
 )
 
 const (
+	CONF = "conf"
 	INI = "ini"
 	JSON = "json"
 	XML = "xml"
 	YAML = "yaml"
+	YML = "yml"
 
 	PATH_DELIMITER = "/"
 )
@@ -17,6 +22,7 @@ const (
 var (
 	ErrorNotFound = errors.New("Not found")
 	ErrorIncorrectPath = errors.New("Incorrect path")
+	ErrorUnknownConfigType = errors.New("Unknown config type")
 )
 
 type Config interface {
@@ -32,12 +38,46 @@ type Config interface {
 	LoadValue(path string, value interface{}) (err error)
 }
 
-func ReadConfig(configPath string) (config *Config, err error) {
+func ReadConfig(configPath string) (Config, error) {
 	return ReadTypedConfig(configPath, path.Ext(configPath))
 }
 
-func ReadTypedConfig(configPath string, configType string) (config *Config, err error) {
-	return nil, nil
+func ReadTypedConfig(configPath string, configType string) (Config, error) {
+	if len(configPath) == 0 {
+		return nil, ErrorIncorrectPath
+	}
+	configFile, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	return ReadConfigFromReader(configFile, configType)
+}
+
+func ReadConfigFromReader(configReader io.Reader, configType string) (Config, error) {
+	configData, err := ioutil.ReadAll(configReader)
+	if err != nil {
+		return nil, err
+	}
+	creator, err := getConfigCreator(configType)
+	if err != nil {
+		return nil, err
+	}
+	return creator(configData)
+}
+
+type configCreator func([]byte) (Config, error)
+
+func getConfigCreator(configType string) (configCreator, error) {
+	creators := map[string]configCreator{
+		CONF: newIniConfig, INI: newIniConfig,
+		JSON: newJsonConfig,
+		XML: newXmlConfig,
+		YAML: newYamlConfig, YML: newYamlConfig}
+
+	if creator, exist := creators[configType]; exist {
+		return creator, nil
+	}
+	return nil, ErrorUnknownConfigType
 }
 
 func splitPath(path string) ([]string) {
